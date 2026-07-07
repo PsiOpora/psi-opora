8b3d35761a4d2de141e2dea1dd35927f6d87ed4a
Maksim <kodermax@gmail.com>
2026-07-07 20:30:50 +0300

    up

--- packages/db/src/index.ts
+++
@@ -1,6 +0,0 @@
-export { alias } from "drizzle-orm/pg-core";
-export * from "drizzle-orm/sql";
-export { type Database, db } from "./client";
-export { db as dbEdge } from "./client.edge";
-export { type DbDriver, selectDbDriver } from "./driver";
-export * from "./schema";
