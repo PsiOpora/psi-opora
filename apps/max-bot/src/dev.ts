b6d931b8ffd3a868ae0420736b95649e0b3c2987
Maksim <kodermax@gmail.com>
2026-07-08 21:32:18 +0300

    up

--- apps/max-bot/src/dev.ts
+++ apps/max-bot/src/dev.ts
@@ -1,6 +1,6 @@
 import { appendFileSync } from "fs";
 import { resolve } from "path";
-import { createMaxBot, log as baseLog } from "./bot.js";
+import { createMaxBot, log as baseLog } from "./bot";
 
 const logPath = resolve("bot.log");
 const log = (msg: string) => {
